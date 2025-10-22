1c145c31ddf0492fc847fc0e6dbdd5d6382a2c4d
AliyaSag <sagdievaaliya05@gmail.com>
2025-10-22 23:19:29 +0300

    Lab 1.04

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -6,28 +6,36 @@
 
 void cg::renderer::rasterization_renderer::init()
 {
+	renderer::load_model();
+	renderer::load_camera();
 	rasterizer = std::make_shared<cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
 	rasterizer->set_viewport(settings->width, settings->height);
 
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(settings->width,settings->height);
 	rasterizer->set_render_target(render_target);
-	model = std:: make_shared<cg::world::model>();
-	model->load_obj(settings->model_path);
-	// TODO Lab: 1.04 Setup an instance of camera `cg::world::camera` class in `cg::renderer::renderer` and `cg::renderer::rasterization_renderer` 
-	// TODO Lab: 1.06 Add depth buffer in `cg::renderer::rasterization_renderer`
+	
 }
 void cg::renderer::rasterization_renderer::render()
 {
-	auto start = std::chrono::high_resolution_clock::now();
+	float4x4 matrix = mul(
+			camera->get_projection_matrix(),
+			camera->get_view_matrix(),
+			model->get_world_matrix()
+	);
+	rasterizer->vertex_shader = [&](float4 vertex, cg::vertex vertex_data){
+		auto processed = mul(matrix, vertex);
+		return std::make_pair(processed, vertex_data);
+	};
 
-	rasterizer->clear_render_target({0, 0, 0});
+	{
+		cg::utils::timer t("Clear");
 
-	auto stop = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<float, std::milli> duration = stop - start;
-	std::cout << "Rasterization time: " << duration.count() << " ms\n";
+		rasterizer->clear_render_target(cg::unsigned_color{
+        	56, 178, 37  
+    	});
+	}
 
-	cg::utils::save_resource(*render_target, settings->result_path);
-	// TODO Lab: 1.04 Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
+	
 	// TODO Lab: 1.05 Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
 	for(size_t shape_id = 0; shape_id < model->get_index_buffers().size(); shape_id++)
 	{
@@ -35,6 +43,7 @@ void cg::renderer::rasterization_renderer::render()
 		rasterizer->set_index_buffer(model->get_index_buffers()[shape_id]);
 		rasterizer->draw(model->get_index_buffers()[shape_id]->count(), 0);
 	}
+	cg::utils::save_resource(*render_target, settings->result_path);
 }
 
 void cg::renderer::rasterization_renderer::destroy() {}

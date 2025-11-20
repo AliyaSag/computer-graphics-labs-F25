f63fc910c27e63292409feb71f7a108b82672458
AliyaSag <sagdievaaliya05@gmail.com>
2025-11-20 23:11:55 +0300

    test

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -245,16 +245,25 @@ void cg::renderer::dx12_renderer::create_root_signature(const D3D12_STATIC_SAMPL
 	));
 }
 
-std::filesystem::path cg::renderer::dx12_renderer::get_shader_path()
-{
-	// TODO Lab: 3.05 Compile shaders
-	return "";
+std::filesystem::path cg::renderer::dx12_renderer::get_shader_path() {
+    return settings->shader_path;
 }
 
-ComPtr<ID3DBlob> cg::renderer::dx12_renderer::compile_shader(const std::string& entrypoint, const std::string& target)
-{
-	// TODO Lab: 3.05 Compile shaders
-	return nullptr;
+Microsoft::WRL::ComPtr<ID3DBlob> cg::renderer::dx12_renderer::compile_shader(const std::string& entrypoint,
+                                                                             const std::string& target) {
+    ComPtr<ID3DBlob> shader, error;
+    UINT compile_flags = 0;
+#ifdef DEBUG
+    compile_flags |= D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif 
+
+    HRESULT res = D3DCompileFromFile(get_shader_path().wstring().c_str(), nullptr, nullptr, entrypoint.c_str(), target.c_str(), compile_flags, 0, &shader, &error);
+    if (FAILED(res)) {
+        OutputDebugStringA((char*)error->GetBufferPointer());
+        THROW_IF_FAILED(res);
+    }
+
+    return shader;
 }
 
 void cg::renderer::dx12_renderer::create_pso()
